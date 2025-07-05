f087d4222123d781f9ef12f8757382e9d00c29e6
Balakrishna0809 <rbk464.kcp@gmail.com>
2025-07-05 15:48:47 +0530

    Added a call to welcome function

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 
 	public static void main(String[] args){
 		hiFromMac("Balakrishna");
+		welcome("krish");
 	}
 	public static void hiFromMac(String message){
 		System.out.println("Hello " + message + " - MacBook Air");
